007ed93eda507b8ac54512332c425f177b51d2e0
CodeWithAbdullah-007 <mabdullahanwar85@gmail.com>
2026-03-27 17:57:31 +0500

    Done

---
+++ script.js
@@ -0,0 +1,89 @@
+// let a = 12;
+// let b = 5
+
+// console.log(a + ++a - b--)
+
+
+
+// pre increment  
+// post increment   
+
+
+
+
+let firstName = "Muhammad"
+let lastName = "Hasan"
+
+// "", '', ``
+
+// let fullName = firstName + "" + lastName // one
+// let fullName = firstName.concat(" " + lastName) // two
+// let fullName = `${firstName} ${lastName}` // three
+
+// console.log(fullName)
+
+
+// let result = 2 + 2 + 1334578
+// console.log(result.slice(0 , 2))
+
+
+
+
+
+// conditional statement
+
+
+// job requirements ==>   age => 18 , location == karachi , exp == 1, lang == js , qualification == 'matric'
+
+
+// let age = 18;
+// let city = "Karachi"
+// let exp = 1;
+// let qualification = "matric"
+// let lang = "JavaScript"
+
+
+// if(age >= 18){
+// if(city == "Karachi"){
+//   if(exp == 1){
+//       if(lang == "JavaScript"){
+//            if(qualification == "matric"){
+// message(`You Are Elegible for this job!
+//     Congratulations!
+//     `)
+// }else {
+//     message("beta board ke paper ki tayyari kero.")
+// }
+// }else {
+//     message("js seekhloo.")
+// }
+
+// }else {
+//     message("Experience Kerlo life mai.")
+// }
+// }else {
+//     message("Karachi Ajaou")
+// }
+// }else{
+//     message("barry hoker aou")
+// }
+
+
+
+// function message (m){
+//     console.log(m)
+// }
+
+
+
+
+let arr = [12, false, "string", 45, true, {name : "hasan"}]
+
+// arr[5].name = "shakoor"
+
+
+
+
+
+console.log(arr.slice(0,2))
+// console.log(arr)
